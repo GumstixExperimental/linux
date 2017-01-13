96116bd15f6818acaf4aff08a8b789d20b308e77
Jerry Hung <jerry.hung@gumstix>
2017-01-13 09:18:29 -0800

    Omap4 : Fixing the poweroff issues for Duovero
    Issue command "poweroff" results in halt. Fix this error and make sure
    system is shut down.
    
    Credits to 29d43c2a05c6281d668e9caf95804c3e21c8a35a
    
    Singed-off-by : Jerry Hung <jerry@gumstix.com>

--- drivers/mfd/twl-core.c
+++ drivers/mfd/twl-core.c
@@ -116,6 +116,10 @@
 #define PWR_P1_SW_EVENTS 		0x10
 #define PWR_DEVOFF	 		(1 << 0)
 
+#define TWL6030_PHOENIX_DEV_ON	0x25
+#define TWL6030_APP_DEVOFF	(1<<0)
+#define TWL6030_CON_DEVOFF	(1<<1)
+#define TWL6030_MOD_DEVOFF	(1<<2)
 /* Triton Core internal information (END) */
 
 
@@ -1084,7 +1088,7 @@ static int twl_remove(struct i2c_client *client)
 	return 0;
 }
 
-static void twl_poweroff(void)
+static void twl_4030_poweroff(void)
 {
 	int err;
 	u8 val;
@@ -1113,6 +1117,36 @@ static struct of_dev_auxdata twl_auxdata_lookup[] = {
 	OF_DEV_AUXDATA("ti,twl4030-gpio", 0, "twl4030-gpio", NULL),
 	{ /* sentinel */ },
 };
+
+static void twl_6030_poweroff(void)
+{
+	int err;
+	u8 val;
+
+	err = twl_i2c_read_u8(TWL6030_MODULE_ID0, &val,
+	                      TWL6030_PHOENIX_DEV_ON);
+	if (err) {
+		pr_err("%s: i2c error %d while reading TWL6030"
+		       "MODULE_ID0 PHOENIX_DEV_ON\n",
+		       DRIVER_NAME, err);
+		return;
+	}
+
+	val |= TWL6030_APP_DEVOFF;
+	val |= TWL6030_CON_DEVOFF;
+	val |= TWL6030_MOD_DEVOFF;
+
+	err = twl_i2c_write_u8(TWL6030_MODULE_ID0, val,
+	                       TWL6030_PHOENIX_DEV_ON);
+
+	if (err) {
+		pr_err("%s: i2c error %d while writing TWL6030"
+		       "MODULE_ID0 PHOENIX_DEV_ON\n",
+		       DRIVER_NAME, err);
+	}
+
+	return;
+}
 
 /* NOTE: This driver only handles a single twl4030/tps659x0 chip */
 static int
@@ -1263,10 +1297,13 @@ twl_probe(struct i2c_client *client, const struct i2c_device_id *id)
 				 TWL4030_DCDC_GLOBAL_CFG);
 	}
 
-	if(twl_has_poweroff())
+	if (twl_has_poweroff())
 	{
 		/* initialize pm_power_off routine */
-		pm_power_off = twl_poweroff;
+		if (twl_class_is_6030())
+			pm_power_off = twl_6030_poweroff;
+		else
+			pm_power_off = twl_4030_poweroff;
 	}
 
 	if (node) {
